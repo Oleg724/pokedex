fc0d8c6f9d816a0c0a913b5e005918220790e76a
Oleg <7241984@gmail.com>
2020-10-09 07:35:03 +0300

    added basename attribute

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -24,7 +24,7 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <PokeapiServiceProvider value={ this.state.pokeapiService }>
-          <Router basename="/pokedex">
+          <Router basename="/pokedex">         
             <div className="app">
               <Header />
               <Switch>
