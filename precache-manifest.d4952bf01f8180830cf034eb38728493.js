5db0f1ac7e8754d6edd7638a5a246baf62c36f50
Oleg <7241984@gmail.com>
2020-10-09 06:11:44 +0300

    Updates

---
+++ precache-manifest.d4952bf01f8180830cf034eb38728493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c312e440b35c1ad54bd5b22eb5edd0f",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "257f6ac475ab236a1519",
+    "url": "/pokedex/static/css/main.babb3dac.chunk.css"
+  },
+  {
+    "revision": "bbb4027b1689ddca0976",
+    "url": "/pokedex/static/js/2.c5262789.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.c5262789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257f6ac475ab236a1519",
+    "url": "/pokedex/static/js/main.9120bc53.chunk.js"
+  },
+  {
+    "revision": "dd6bddbc3b7842f642db",
+    "url": "/pokedex/static/js/runtime-main.ed7addec.js"
+  }
+]);
